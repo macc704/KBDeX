c7f9f54e39eeaf8c2593529ffdb228b02dbba80c
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2014-11-17 09:46:44 -0500

    Student Co-Authorship Converter

--- KBDeX/src/kbdex/app/ConverterMain.java
+++ KBDeX/src/kbdex/app/ConverterMain.java
@@ -1,32 +1,42 @@
 package kbdex.app;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import clib.common.filesystem.CFile;
 import clib.common.filesystem.CFileSystem;
 import clib.common.table.CCSVFileIO;
 
+/**
+ * Converter for to divide CoauthorShip note into two
+ */
 public class ConverterMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		new ConverterMain().run();
 	}
-	
+
 	private String infile = "in.csv";
 	private String outfile = "data.csv";
-	
-	private int studentRow = 2;
-	private int contentRow = 8;
 
-	void run() {
+	private int studentRow = 9;
+	private int contentRow = 13;
+	private int timeRow = 3;
+
+	//02/05/2001 11:32:27
+	//<code>"EEE, d MMM yyyy HH:mm:ss Z"</code>
+	private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+
+	void run() throws Exception{
 		String previousContent = "";
 
 		CFile in = CFileSystem.getExecuteDirectory().findOrCreateFile(infile);
 		String[][] tableIn = CCSVFileIO.load(in);
 		List<List<String>> tableOut = new ArrayList<List<String>>();
 		int counter = -1;
-		long time = 1350270000000L;
 		for (String[] rowIn : tableIn) {
 			counter++;
 			if (counter == 0) {
@@ -40,8 +50,11 @@ public class ConverterMain {
 				continue;
 			}
 
-			time = time + 1000;
 			String content = rowIn[contentRow - 1];
+			content = content.replaceAll(":}", ":} ");
+
+			String timeStr = rowIn[timeRow - 1];
+			Date d = dateFormat.parse(timeStr);
 
 			if (previousContent.equals(content)) {
 				System.out.println("the same content id=" + counter);
@@ -59,7 +72,7 @@ public class ConverterMain {
 				rowOut.add(student);
 				rowOut.add(content);
 				rowOut.add("Default Group");
-				rowOut.add(Long.toString(time));
+				rowOut.add(Long.toString(d.getTime()));
 				tableOut.add(rowOut);
 				previousContent = content;
 			}
