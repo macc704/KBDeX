37ddff5b81de629450137e7d0663b5903fc14fd9
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2015-01-13 14:54:24 -0500

    Added some arrangement of the database name setting

--- KFDataExporter/src/kfl/app/kf6exporter/KF6ConverterMainTest.java
+++ KFDataExporter/src/kfl/app/kf6exporter/KF6ConverterMainTest.java
@@ -2,7 +2,8 @@ package kfl.app.kf6exporter;
 
 public class KF6ConverterMainTest {
 	public static void main(String[] args) throws Exception{
-		//Susana_test-20150113-142045
 		KF6ConverterMain.main(new String[]{"Susana_test-20150113-143324"});
+		//KF6ConverterMain.main(new String[]{"KSN-20150113-143724"});
+		//KF6ConverterMain.main(new String[]{"ICS Lab School-20150113-144717"});
 	}
 }
--- KFDataExporter/src/kfl/app/serializer/KF4AttachmentDownloaderMainTest.java
+++ KFDataExporter/src/kfl/app/serializer/KF4AttachmentDownloaderMainTest.java
@@ -3,8 +3,9 @@ package kfl.app.serializer;
 public class KF4AttachmentDownloaderMainTest {
 
 	public static void main(String[] args) throws Exception {
-		// Susana_test-20150113-142045
+		// KF4AttachmentDownloaderMain
+		// .main(new String[] { "Susana_test-20150113-142045" });
 		KF4AttachmentDownloaderMain
-				.main(new String[] { "Susana_test-20150113-143324" });
+				.main(new String[] { "ICS Lab School-20150113-144717" });
 	}
 }
