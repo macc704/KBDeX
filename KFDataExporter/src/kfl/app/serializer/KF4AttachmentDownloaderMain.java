6130187c8aff152647fe9d65346e609151368df1
Yoshiaki Matsuzawa <yoshiaki.matsuzawa@gmail.com>
2015-01-13 14:39:10 -0500

    Added missing file related to the previous commit.

---
+++ KFDataExporter/src/kfl/app/serializer/KF4AttachmentDownloaderMain.java
@@ -0,0 +1,35 @@
+package kfl.app.serializer;
+
+import kfl.kf4.serializer.KFAllAttachmentDownloader;
+import kfl.kf4.serializer.KFSerializeFolder;
+import clib.common.filesystem.CDirectory;
+import clib.common.filesystem.CFileSystem;
+
+public class KF4AttachmentDownloaderMain {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.out.println("Args length has to be 1.");
+			return;
+		}
+		new KF4AttachmentDownloaderMain().run(args[0]);
+	}
+
+	void run(String name) throws Exception {
+		CDirectory baseDir = CFileSystem.getExecuteDirectory()
+				.findOrCreateDirectory("kf.out");
+		CDirectory dir = baseDir.findDirectory(name);
+		if (dir == null) {
+			System.out.println("database " + name + " is not found.");
+			return;
+		}
+		process(dir);
+	}
+
+	void process(CDirectory dir) throws Exception {
+		KFSerializeFolder folder = new KFSerializeFolder(dir);
+		folder.loadMeta();
+		new KFAllAttachmentDownloader().start(folder);
+	}
+
+}
---
+++ KFDataExporter/src/kfl/app/serializer/KF4AttachmentDownloaderMainTest.java
@@ -0,0 +1,10 @@
+package kfl.app.serializer;
+
+public class KF4AttachmentDownloaderMainTest {
+
+	public static void main(String[] args) throws Exception {
+		// Susana_test-20150113-142045
+		KF4AttachmentDownloaderMain
+				.main(new String[] { "Susana_test-20150113-143324" });
+	}
+}
